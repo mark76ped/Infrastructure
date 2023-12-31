fb0d28675d34a82f222101c67275928dcf6c91b8
Mark Pedersen <markpedersen@Marks-MBP.localdomain>
2023-12-31 16:09:50 +0100

    rust addition

---
+++ rust/controlflowexample/target/debug/deps/controlflowexample-2b788793e22c4988.d
@@ -0,0 +1,5 @@
+/Users/markpedersen/Infrastructure/Infrastructure/rust/controlflowexample/target/debug/deps/controlflowexample-2b788793e22c4988: src/main.rs
+
+/Users/markpedersen/Infrastructure/Infrastructure/rust/controlflowexample/target/debug/deps/controlflowexample-2b788793e22c4988.d: src/main.rs
+
+src/main.rs:
---
+++ rust/rust_project/target/debug/deps/rust_project-0bb1e234f8afca1c.d
@@ -0,0 +1,7 @@
+/Users/markpedersen/Infrastructure/Infrastructure/rust/rust_project/target/debug/deps/librust_project-0bb1e234f8afca1c.rmeta: src/lib.rs
+
+/Users/markpedersen/Infrastructure/Infrastructure/rust/rust_project/target/debug/deps/librust_project-0bb1e234f8afca1c.rlib: src/lib.rs
+
+/Users/markpedersen/Infrastructure/Infrastructure/rust/rust_project/target/debug/deps/rust_project-0bb1e234f8afca1c.d: src/lib.rs
+
+src/lib.rs:
