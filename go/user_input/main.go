825e90498c65418f6e82ad24e475bcdf7f692937
mark76ped <markpedersen76@gmail.com>
2024-09-26 16:00:25 +0200

    add more to user_input

--- go/user_input/main.go
+++ go/user_input/main.go
@@ -7,11 +7,37 @@ import (
 	"strings"
 
 	"example.com/note/note"
+	"example.com/note/todo"
 )
+
+type saver interface {
+	Save() error
+}
+
+type displayer interface {
+	Display()
+}
+
+type outputtable interface {
+	saver
+	displayer
+}
+
+// type outputtable interface {
+// 	Save() error
+// 	Display()
+// }
 
 func main() {
 	title, content:= getNoteData()
+	todoText := getUserInput("Todo text: ")
 
+	todo, err := todo.New(todoText)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	userNote, err := note.New(title, content)
 
 	if err != nil {
@@ -19,17 +45,60 @@ func main() {
 		return
 	}
 
-	userNote.Display()
-	err = userNote.Save()
+	err = outputData(todo)
 
-	if err != nil {
-		fmt.Println("Saving the note failed!")
+	err = outputData(userNote)
+}
+
+
+func printSomething(value interface{}) { //any value is allowed with interface{}
+
+	typedVal, ok := value.(string)
+	if !ok {
+		fmt.Println("It does not work", typedVal)
 		return
 	}
 
-	fmt.Println("Saving the note succeeded!")
+	intVal, ok := value.(int)
+	if !ok {
+		fmt.Println("It does not work", intVal)
+		return
+	}
+	floatVal, ok := value.(float64)
+	if !ok {
+		fmt.Println("It does not work", floatVal)
+		return
+	}
+	// switch value.(type) {
+	// case int: 
+	// 	fmt.Println("Integer:", value)
+	// case float64:
+	// 	fmt.Println("Float:", value)
+	// case string:
+	// 	fmt.Println(value)
+	// default:
+	// 	fmt.Println("Type must be an Integer, Float, or String.")
+	// }
+	// fmt.Println(value)
 }
 
+func outputData(data outputtable) error {
+	data.Display()
+	saveData(data)
+	return nil
+}
+
+func saveData(data saver) error{
+	err := data.Save()
+
+	if err != nil {
+		fmt.Println("Saving the note failed!")
+		return err
+	}
+
+	fmt.Println("Saving the note succeeded!")
+	return nil
+}
 
 func getNoteData() (string, string) {
 	title := getUserInput("Note title:")
